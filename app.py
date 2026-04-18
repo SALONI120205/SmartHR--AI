34074e688bc37bf85a7aea3d005fb54cd3e6a507
Saloni Gajwe <salonigajwe12@gmail.com>
2026-04-18 14:03:20 +0530

    update

--- app.py
+++ app.py
@@ -16,10 +16,29 @@ def home():
 def employees():
     return render_template('employees.html')
 
-@app.route('/resume')
-def resume():
-    return render_template('resume.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/attendance')
+def attendance():
+    return render_template('attendance.html')
+
+@app.route('/salary')
+def salary():
+    return render_template('salary.html')
+
+@app.route('/meetings')
+def meetings():
+    return render_template('meetings.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
